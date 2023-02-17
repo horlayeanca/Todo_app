63e254170a2d5ee20b2f77d7f73d2f6357bfa8d6
Faruq Adelodun <adelodunfaruq@gmail.com>
2023-02-17 17:57:27 +0100

    added theme button

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ window.addEventListener("load", () => {
   const input = document.querySelector("#new-task-input");
   const list_el = document.querySelector("#tasks");
 
+  let isThemeDark = false;
+
+  const toggleBtnEl = document.querySelector(".theme-toggle--button");
+
+  const backgroundEl = document.querySelector(".background");
+
+  const shapeEl = document.querySelector(".shape");
+
+  toggleBtnEl.addEventListener("click", toggleTheme);
+
+  function toggleTheme() {
+    isThemeDark = !isThemeDark;
+    const replacedClass = isThemeDark ? "moon" : "sun";
+    const replacedWithClass = isThemeDark ? "sun" : "moon";
+    shapeEl.classList.replace(replacedClass, replacedWithClass);
+    backgroundEl.classList.toggle("dark");
+  }
+
   form.addEventListener("submit", (e) => {
     e.preventDefault();
 
